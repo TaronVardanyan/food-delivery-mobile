cca05a47a72dda101c11d944225171990dc46c3a
TaronVardanyan <tarokavardanyan@gmail.com>
2025-09-29 20:11:54 +0400

    add assets, constants

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,28 @@
-import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { Stack, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import './global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  // useEffect(() => {
+  //   fetchAuthenticatedUser()
+  // }, []);
+
+  if(!fontsLoaded) return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
